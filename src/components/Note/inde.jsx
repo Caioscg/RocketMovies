3df034dc5fc816902020c383283c61aca4865916
Caioscg <cacazaok5@gmail.com>
2023-04-27 18:49:59 -0300

    note

--- src/components/Note/inde.jsx
+++ src/components/Note/inde.jsx
@@ -1,9 +1,23 @@
 import { Container } from "./styles";
+import { Star } from "../Star"
+import { Tags } from "../Tags"
 
-export function Note() {
+export function Note({data, ...rest}) {
     return(
-        <Container>
-            
+        <Container {...rest}>
+            <h1>{data.title}</h1>
+            <Star number={data.starNumber} size="20"/>
+            <p>
+                {data.description}
+            </p>
+            {
+                data.tags &&
+                <footer>
+                    {
+                        data.tags.map(tag => <Tags key={tag.id} title={tag.name} /> )
+                    }
+                </footer>
+            }
         </Container>
     )
 }
