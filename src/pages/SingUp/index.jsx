7e4a8a57f4339495c1f6d4d88767d65945079bf8
Caioscg <cacazaok5@gmail.com>
2023-07-07 23:45:44 -0300

    sign up

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
 import { Container, Form, Background } from "./styles";
 
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi'
@@ -7,6 +11,31 @@ import { Button } from "../../components/Button";
 import { Link } from "../../components/Link";
 
 export function SingUp() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert("Preencha todos os campos!")
+        }
+
+        api.post("/users", { name, email, password })
+        .then(() => {
+            alert("Usuário cadastrado com sucesso!")
+            navigate("/")
+        })
+        .catch(error => {
+            if(error.response) {
+                alert(error.response.data.message)  // tratamento feito no back-end
+            } else {
+                alert("Não foi possível cadastrar")
+            }
+        })
+    }
+
     return(
         <Container>
             <Background />
@@ -24,21 +53,24 @@ export function SingUp() {
                     placeholder="Nome"
                     icon={FiUser}
                     type="text"
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input 
                     placeholder="E-mail"
                     icon={FiMail}
                     type="text"
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input 
                     placeholder="Senha"
                     icon={FiLock}
                     type="password"
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar"/>
+                <Button title="Cadastrar" onClick={handleSignUp} />
 
                 <Link to="/" content="Voltar para o login" icon={FiArrowLeft}/>
             </Form>
