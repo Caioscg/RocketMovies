8a273776e7ccb305630402339f2309a8c424ff1f
Caioscg <cacazaok5@gmail.com>
2023-07-19 19:33:07 -0300

    connecting backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios" // npm install axios (trabalha com requisições HTTP (get, post...))
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "https://rocketmotes-api.onrender.com"
 })
-
-// quando vai usar , so usa api.get("/user") ai ja vai ter o localhost:3333
